73dc7159181f1b8622aa083c17b20c8eafc39915
Viraj Shah <virajshah@users.noreply.github.com>
2014-08-19 14:31:59 -0700

    Added Dyno Testing Folder for test data and final DAQ acquisition files

---
+++ Dyno Testing/thermistorNxpKTY84.py
@@ -0,0 +1,21 @@
+#Using the lookup table generated from \\valentino\engineering\Dyno\Dyno 2013\OEM Components\Sensors\Temperature\Thermistor Table - Updated.xlsx
+
+#Returns T in Celsius.
+
+Resistance_LookUp_Ohms = (359,391,424,460,498,538,581,626,672,722,773,826,882,940,1000,1062,1127,1194,1262,1334,1407,1482,1560,1640,1722)
+
+Temperature_LookUp_C = (-40,-30,-20,-10,0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200) 
+
+def voltsToTemp(measuredV, supplyV, pullupR):
+	thermistorR = 0 if (measuredV == 0 or supplyV == 0 or measuredV == supplyV) else pullupR/((float(supplyV)/measuredV) - 1) #Voltage divider formula
+	indexForInterp = 0
+	for i in range(len(Resistance_LookUp_Ohms)):
+		if thermistorR <= Resistance_LookUp_Ohms[i]:
+			indexForInterp = i -1
+			r_pt1 = Resistance_LookUp_Ohms[indexForInterp]
+			r_pt2 = Resistance_LookUp_Ohms[indexForInterp+1]
+			temp_pt1 = Temperature_LookUp_C[indexForInterp]
+			temp_pt2 = Temperature_LookUp_C[indexForInterp + 1]
+			m = (temp_pt2 - temp_pt1)/float((r_pt2 - r_pt1))
+			b = temp_pt1 - (m*r_pt1)
+			return (m*thermistorR) + b
---
+++ Dyno Testing/thermistorOmega44004.py
@@ -0,0 +1,21 @@
+#Temperature calculation based on voltage measurement form DAQ. 
+#Using datasheet: http://www.omega.com/Temperature/pdf/44000_THERMIS_ELEMENTS.pdf
+#This is using a pullup resistor.
+
+#Returns T in Celsius.
+
+from math import log #imports natural log (a.k.a. ln)
+
+def voltsToTemp(measuredV, supplyV, pullupR):
+	#Steinhart-Hart Equation coefficients
+	A = 1.468E-3
+	B = 2.383E-4
+	C = 1.007E-7
+
+	thermistorR = 0 if (measuredV == 0 or supplyV == 0 or measuredV == supplyV) else pullupR/((float(supplyV)/measuredV) - 1) #Voltage divider formula
+	try:
+		inverseT = A + B*log(thermistorR) + C*(log(thermistorR)**3)
+	except:
+		inverseT = 1
+
+	return (float(1)/inverseT)-273.15
